138ec5e93ce911fbe2109f95b2f6b8a8c5b82bbc
akshkalyani <akshkalyani@gmail.com>
2023-03-27 16:05:37 +0530

    commit to save

---
+++ Homify1/Homify1/Views/disposalTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  disposalTableViewCell.swift
+//  Homify1
+//
+//  Created by aksh kalyani on 27/03/23.
+//
+
+import UIKit
+
+class disposalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        view.backgroundColor = .red
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
